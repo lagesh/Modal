4dc681b6977f850700649d656e5392d106fa8e94
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-05-03 16:16:59 +0100

    add js interactivity

---
+++ script.js
@@ -0,0 +1,25 @@
+//the modal
+var modal = document.getElementById("myModal");
+
+//the button element that opnes up modal
+var btn = document.getElementById("modalButton");
+
+//span element that closes modal
+var span = document.querySelector('.close');
+
+//when button is clicked, open modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+}
+
+//when span(x) is clicked, close modal
+span.onclick = function () {
+  modal.style.display = 'none';
+}
+
+//when anywhere outside of modal is clicked, close modal
+window.onclick = function (e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+}
